ca37e08b8e8460baf4b946be7a9aa10ea210a952
Aaron Tagadtad <aaron@alias.com>
2021-05-21 09:55:55 -0400

    changed to add new part

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -36,7 +36,8 @@
             class="btn btn-default bg-transparent hover:bg-primary text-primary hover:text-white border border-primary hover:border-transparent inline-flex items-center relative mr-3"
             @click="addItem()"
           >
-            Add new {{ field.singularLabel.toLowerCase() }}
+            <!-- Add new {{ field.singularLabel.toLowerCase() }} -->
+            Add New Part
           </button>
         </div>
       </div>
